9f0ebf4a2e13e8dd53975766827a41f527554429
Ali Somay <alisomay@runbox.com>
2022-08-15 11:12:55 +0200

    Update harriet on plow graphify but not implement new features of it yet

--- plow_graphify/src/lib.rs
+++ plow_graphify/src/lib.rs
@@ -57,8 +57,8 @@
 )]
 
 use harriet::{
-    Directive, IRIReference, Item, Literal, Object, PrefixedName, Statement as HarrietStatement,
-    Subject, Triples, TurtleDocument, IRI as HarrietIRI,
+    Directive, IRIReference, Literal, Object, PrefixedName, Statement as HarrietStatement, Subject,
+    Triples, TurtleDocument, Verb, IRI as HarrietIRI,
 };
 use rdftk_core::model::graph::GraphRef;
 use rdftk_core::model::literal::LanguageTag;
@@ -90,72 +90,30 @@ pub fn document_to_graph(document: &TurtleDocument) -> Result<GraphRef, RDFParse
 
     let mut prefix_map: HashMap<Option<String>, IRIReference> = HashMap::new();
 
-    for item in &document.items {
-        match item {
-            Item::Statement(statement) => match statement {
-                HarrietStatement::Directive(directive) => {
-                    // TODO: Ignoring @base, etc. for now
-                    if let Directive::Prefix(prefix) = directive {
-                        prefix_map.insert(
-                            prefix.prefix.as_ref().map(ToString::to_string),
-                            prefix.iri.clone(),
-                        );
-                    }
+    for statement in &document.statements {
+        match statement {
+            HarrietStatement::Directive(directive) => {
+                // TODO: Ignoring @base, etc. for now
+                if let Directive::Prefix(prefix) = directive {
+                    prefix_map.insert(
+                        prefix.prefix.as_ref().map(ToString::to_string),
+                        prefix.iri.clone(),
+                    );
                 }
-                HarrietStatement::Triples(triples) => match triples {
-                    Triples::Labeled(subject, predicate_object_list) => match subject {
-                        Subject::IRI(subject) => {
-                            let subject = resolve_iri(&prefix_map, subject)?;
-                            for (predicate, object_list) in &predicate_object_list.list {
-                                let predicate = resolve_iri(&prefix_map, predicate)?;
-                                for object in &object_list.list {
-                                    match object {
-                                        Object::IRI(object) => {
-                                            let object = resolve_iri(&prefix_map, object)?;
-                                            // We know the input is valid because `harriet_to_rdftk_iri` only accepts valid input.
-                                            #[allow(clippy::unwrap_used)]
-                                            statements.push(
-                                                factory
-                                                    .statement(
-                                                        factory.named_subject(
-                                                            harriet_to_rdftk_iri(&subject).into(),
-                                                        ),
-                                                        harriet_to_rdftk_iri(&predicate).into(),
-                                                        factory.named_object(
-                                                            harriet_to_rdftk_iri(&object).into(),
-                                                        ),
-                                                    )
-                                                    .unwrap(),
-                                            );
-                                        }
-                                        Object::Literal(literal) => match literal {
-                                            Literal::RDFLiteral(rdf_literal) => {
-                                                let string = rdf_literal.string.to_string();
-                                                let object_node: ObjectNodeRef;
-                                                if let Some(ref language_tag) =
-                                                    rdf_literal.language_tag
-                                                {
-                                                    // Does not check or report errors on malformed language tags
-                                                    // Instead it would just ignore it.
-                                                    if let Ok(tag) =
-                                                        LanguageTag::from_str(language_tag)
-                                                    {
-                                                        object_node = factory.literal_object(
-                                                            literal_factory
-                                                                .with_language(&string, tag),
-                                                        );
-                                                    } else {
-                                                        object_node = factory.literal_object(
-                                                            literal_factory.string(&string),
-                                                        );
-                                                    }
-                                                } else {
-                                                    object_node = factory.literal_object(
-                                                        literal_factory.string(&string),
-                                                    );
-                                                }
+            }
+            HarrietStatement::Triples(triples) => match triples {
+                Triples::Labeled(_, subject, predicate_object_list) => match subject {
+                    Subject::IRI(subject) => {
+                        let subject = resolve_iri(&prefix_map, subject)?;
+                        for (_, verb, object_list, _) in &predicate_object_list.list {
+                            match verb {
+                                Verb::IRI(predicate) => {
+                                    let predicate = resolve_iri(&prefix_map, predicate)?;
+                                    for (_, _, object) in &object_list.list {
+                                        match object {
+                                            Object::IRI(object) => {
+                                                let object = resolve_iri(&prefix_map, object)?;
                                                 // We know the input is valid because `harriet_to_rdftk_iri` only accepts valid input.
-                                                // Also `object_node` variable will always be a valid `ObjectNodeRef`.
                                                 #[allow(clippy::unwrap_used)]
                                                 statements.push(
                                                     factory
@@ -165,31 +123,83 @@ pub fn document_to_graph(document: &TurtleDocument) -> Result<GraphRef, RDFParse
                                                                     .into(),
                                                             ),
                                                             harriet_to_rdftk_iri(&predicate).into(),
-                                                            object_node,
+                                                            factory.named_object(
+                                                                harriet_to_rdftk_iri(&object)
+                                                                    .into(),
+                                                            ),
                                                         )
                                                         .unwrap(),
                                                 );
                                             }
-                                            Literal::BooleanLiteral(_) => {
-                                                return Err(RDFParseError::UnsupportedStructure);
+                                            Object::Literal(literal) => match literal {
+                                                Literal::RDFLiteral(rdf_literal) => {
+                                                    let string = rdf_literal.string.to_string();
+                                                    let object_node: ObjectNodeRef;
+                                                    if let Some(ref language_tag) =
+                                                        rdf_literal.language_tag
+                                                    {
+                                                        // Does not check or report errors on malformed language tags
+                                                        // Instead it would just ignore it.
+                                                        if let Ok(tag) =
+                                                            LanguageTag::from_str(language_tag)
+                                                        {
+                                                            object_node = factory.literal_object(
+                                                                literal_factory
+                                                                    .with_language(&string, tag),
+                                                            );
+                                                        } else {
+                                                            object_node = factory.literal_object(
+                                                                literal_factory.string(&string),
+                                                            );
+                                                        }
+                                                    } else {
+                                                        object_node = factory.literal_object(
+                                                            literal_factory.string(&string),
+                                                        );
+                                                    }
+                                                    // We know the input is valid because `harriet_to_rdftk_iri` only accepts valid input.
+                                                    // Also `object_node` variable will always be a valid `ObjectNodeRef`.
+                                                    #[allow(clippy::unwrap_used)]
+                                                    statements.push(
+                                                        factory
+                                                            .statement(
+                                                                factory.named_subject(
+                                                                    harriet_to_rdftk_iri(&subject)
+                                                                        .into(),
+                                                                ),
+                                                                harriet_to_rdftk_iri(&predicate)
+                                                                    .into(),
+                                                                object_node,
+                                                            )
+                                                            .unwrap(),
+                                                    );
+                                                }
+                                                Literal::BooleanLiteral(_)
+                                                | Literal::NumericLiteral(_) => {
+                                                    return Err(
+                                                        RDFParseError::UnsupportedStructure,
+                                                    );
+                                                }
+                                            },
+                                            _ => {
+                                                // TODO
+                                                // return Err(RDFParseError::UnsupportedStructure);
                                             }
-                                        },
-                                        _ => {
-                                            // TODO
-                                            // return Err(RDFParseError::UnsupportedStructure);
                                         }
                                     }
                                 }
+                                Verb::A => {
+                                    return Err(RDFParseError::UnsupportedStructure);
+                                }
                             }
                         }
-                        Subject::Collection(_) => {
-                            // TODO
-                            return Err(RDFParseError::UnsupportedStructure);
-                        }
-                    },
+                    }
+                    Subject::Collection(_) | Subject::BlankNode(_) => {
+                        // TODO
+                        return Err(RDFParseError::UnsupportedStructure);
+                    }
                 },
             },
-            Item::Comment(_) => {}
         }
     }
 
